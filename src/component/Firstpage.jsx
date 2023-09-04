18b1188a4f38d42402192abbe6939764805ca99f
shubhwebdev <sc5112492@gmail.com>
2023-09-04 10:40:09 +0530

    sss

--- src/component/Firstpage.jsx
+++ src/component/Firstpage.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { useState } from 'react';
 import { Link } from "react-router-dom";
+import LazyImage from './LazyImage';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
+import 'react-lazy-load-image-component/src/effects/black-and-white.css';
+// import './Home/blur.css'
 
 
 const Firstpage = ({ isToggled, toggleState }) => {
@@ -8,24 +14,29 @@ const Firstpage = ({ isToggled, toggleState }) => {
 
  
   return (
-    <div className='  fixed  w-screen z-10 top-0 bg-white h-full'>
+    <div className='  fixed  w-screen z-10 top-0 bg-white h-[100vh]'>
 
-    <div className=' grid grid-cols-3   lg:grid-cols-5 h-full overflow-hidden '>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
+    <div className=' grid grid-cols-3   lg:grid-cols-5 h-full grid-rows-3 overflow-hidden '>
+        <div className=' overflow-hidden h-full w-full'><LazyLoadImage   src="https://res.cloudinary.com/dmhzsmx4p/image/upload/v1688558449/k7l5mzsbwus7p5i0btsp.jpg"  className=" object-cover w-full h-full object-center " /></div>
+        <div className=' overflow-hidden h-full w-full'><LazyLoadImage src="https://res.cloudinary.com/dmhzsmx4p/image/upload/v1693391924/yxh3nx6busspojuhssf1.jpg"  className=" object-cover w-full h-full object-center " /></div>
+        <div className=' overflow-hidden h-full w-full'><LazyLoadImage src="https://res.cloudinary.com/dmhzsmx4p/image/upload/v1693379961/avsxobvkouznbboeqk4o.jpg"  className=" object-cover w-full h-full object-center " /></div>
+        <div className=' overflow-hidden h-full w-full'><LazyLoadImage src="https://res.cloudinary.com/de0baexee/image/upload/v1691156974/lozkvsppeyvc4g8jrzz0.jpg"  className=" object-cover w-full h-full object-center " /></div>
+        <div className=' overflow-hidden h-[100%] w-full'><LazyLoadImage src="https://res.cloudinary.com/de0baexee/image/upload/v1691157226/pcuph4x83glyppkrxpql.jpg" className=" object-cover w-full h-full object-center " /></div>
+     
+    
+       
+        <div className=' overflow-hidden h-full w-full'><LazyLoadImage   src="https://res.cloudinary.com/dmhzsmx4p/image/upload/v1693382651/iptt9zxzts9e1l8scsvv.jpg"  className=" object-cover w-full h-full object-top " /></div>
+        <div className=' overflow-hidden h-full w-full'><LazyLoadImage   src="https://res.cloudinary.com/de0baexee/image/upload/v1691295937/ftto9m6ebjarpivks3vi.jpg"  className=" object-cover w-full h-full object-top " /></div>
+      
         <div className=' bg-black text-white flex justify-center items-center text-2xl '> <Link className=' border-2 px-5 hover:bg-white hover:text-black' to={'/home'} onClick={toggleState}>Enter</Link></div>
-        <div>9</div>
-        <div>10</div>
-        <div>11</div>
-        <div>12</div>
-        <div>13</div>
-        <div>14</div>
-        <div>15</div>
+        <div className=' overflow-hidden h-full w-full'><LazyLoadImage   src="https://res.cloudinary.com/de0baexee/image/upload/v1691156604/ypggmwkynfx3hsfvnmyn.jpg"  className=" object-cover w-full h-full object-bottom " /></div>
+        <div className=' overflow-hidden h-full w-full'><LazyLoadImage   src="https://res.cloudinary.com/de0baexee/image/upload/v1691156718/mzepawlpry66gjemfya7.jpg"  className=" object-cover w-full h-full object-bottom " /></div>
+       
+        <div className=' overflow-hidden h-full w-full'><LazyLoadImage   src="https://res.cloudinary.com/de0baexee/image/upload/v1691156724/njwf5rkxzcgyx0kvzpoj.jpg"  className=" object-cover w-full h-full object-center " /></div>
+        <div className=' overflow-hidden h-full w-full'><LazyLoadImage src="https://res.cloudinary.com/dmhzsmx4p/image/upload/v1693391924/yxh3nx6busspojuhssf1.jpg"  className=" object-cover w-full h-full object-center " /></div>
+        <div className=' overflow-hidden h-full w-full'><LazyLoadImage src="https://res.cloudinary.com/dmhzsmx4p/image/upload/v1693379961/avsxobvkouznbboeqk4o.jpg"  className=" object-cover w-full h-full object-center " /></div>
+        <div className=' overflow-hidden h-full w-full'><LazyLoadImage src="https://res.cloudinary.com/de0baexee/image/upload/v1691156974/lozkvsppeyvc4g8jrzz0.jpg"  className=" object-cover w-full h-full object-center " /></div>
+        <div className=' overflow-hidden h-[100%] w-full'><LazyLoadImage src="https://res.cloudinary.com/de0baexee/image/upload/v1691157226/pcuph4x83glyppkrxpql.jpg" className=" object-cover w-full h-full object-center " /></div>
     </div>
 
   </div>
--- src/component/LazyImage.jsx
+++ src/component/LazyImage.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
+import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
+import 'react-lazy-load-image-component/src/effects/black-and-white.css';
 
-const LazyImage = ({src,className}) => {
+const LazyImage = ({ src, className }) => {
   return (
-    <div className=' w-full h-full'>
+    <div className={`w-full h-full ${className || ''}`}>
+      <LazyLoadImage
+        className="w-full h-full object-cover object-center"
+        alt=""
+        effect="blur"
+        src={src}
+      />
+    </div>
+  );
+};
 
-<LazyLoadImage
-className={className || " w-full h-full object-cover"}
-alt=""
-effect='blur'
-src={src}
-
-/>
-</div>
-  )
-}
-
-export default LazyImage
+export default LazyImage;
